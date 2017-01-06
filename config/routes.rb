fd0fb7824e29c6161eb55c8ed963604796017df6
Scot-Alan <hedricksa@gmail.com>
2017-01-06 14:28:42 -0600

    added links to navbar and user account function.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
 
   devise_for :users
-  root 'pages#home'
+  root  'pages#home'
   get 'about' => 'pages#about'
+  get 'home' => 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
